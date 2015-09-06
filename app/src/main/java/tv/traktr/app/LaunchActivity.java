7d633158352802b641acf3c42b22dcffdf91fbd5
Michell Bak <michell.bak@gmail.com>
2015-09-06 17:57:30 +0200

    Add initial app project files

---
+++ app/src/main/java/tv/traktr/app/LaunchActivity.java
@@ -0,0 +1,17 @@
+package tv.traktr.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LaunchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Use this Activity to determine if there's
+        // a user logged in or if the sign in screen
+        // should be shown
+    }
+
+}
